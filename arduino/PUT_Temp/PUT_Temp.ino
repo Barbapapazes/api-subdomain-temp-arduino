890fe94166acc632603572ce93f244b2d742179a
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-07-02 15:57:42 +0200

    update the arduino prgm

--- arduino/PUT_Temp/PUT_Temp.ino
+++ arduino/PUT_Temp/PUT_Temp.ino
@@ -17,23 +17,6 @@ void setup() {
     Serial.println("Waiting for connection");
  
   }
-  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
- 
-   HTTPClient http;    //Declare object of class HTTPClient
- 
-   http.begin("http://api.myapp:3000/temp/addSensor");      //Specify request destination
-   http.addHeader("Content-Type", "application/x-www-form-urlencoded");  //Specify content-type header
-
-   String data = "idSensor=chambre";
-   int httpCode = http.POST(data);   //Send the request
-   String payload = http.getString();                  //Get the response payload
- 
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();  //Close connection
- 
- }
 }
 
 void loop() {
@@ -53,11 +36,11 @@ void loop() {
  
    HTTPClient http;    //Declare object of class HTTPClient
  
-   http.begin("http://api.myapp:3000/temp/addTemp");      //Specify request destination
+   http.begin("http://api.myapp:3000/temp");      //Specify request destination
    http.addHeader("Content-Type", "application/x-www-form-urlencoded");  //Specify content-type header
 
-   String data = "idSensor=chambre&temp=" + String(temperature);
-   int httpCode = http.POST(data);   //Send the request
+   String data = "name=chambre-esteban&temp=" + String(temperature);
+   int httpCode = http.PUT(data);   //Send the request
    String payload = http.getString();                  //Get the response payload
  
    Serial.println(httpCode);   //Print HTTP return code
